b6a5b2a221e0a318727767fdd48e1e2e21edc5ce
Kseniya Stavitskaya <stavitskayaks@gmail.com>
2023-04-21 07:32:12 +0300

    task added

---
+++ task6.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+int main(){
+    doublle i1 = 0, i2 = 0, u1 = 0, u2 = 0, r1 = 0, r2 = 0;
+    printf("ВВедите напряжение: "/n);
+    scanf("%lf, &u1");
+    scanf("%lf, &u2");
+    printf("ВВедите сопротивление: "/n);
+    scanf("%lf, &r1");
+    scanf("%lf, &r2");
+    i1 = u1/r1;
+    i2 = u2/r2;
+    if(i1>i2){
+        printf("Сила тока в первой цепи больше,чем во второй");
+    }
+    else{
+        printf("Сила тока во второй цепи больше, чем в первой");
+    }
+    return 0;
+}
